f3abc4bee229bad23b0a44800b0b779c60df4bac
John Collins <collins@braincorporation.com>
2019-09-28 14:57:01 -0700

    Initial impl working. Local java dep for now.

--- src/clj/clj_rosbag/core.clj
+++ src/clj/clj_rosbag/core.clj
@@ -1,42 +1,51 @@
 (ns clj-rosbag.core
   (:require [clojure.java.data :refer [from-java]])
   (:import [com.github.swrirobotics.bags.reader BagFile]
-           [com.github.swrirobotics.bags.reader.messages.serialization BagMessage MessageType]))
+           [com.github.swrirobotics.bags.reader.messages.serialization BagMessage MessageType ArrayType]))
 
-(defrecord RosMessage [topic msg time])
+(defrecord RosMessage [topic message time])
 
-#_(defn parse-field [field-type field]
-  (case field-type
-    "bool" (+ 1)
-    "bool[]"
-    "int8"
-    "int8[]"
-    "uint8"
-    "int16"
-    "uint16"
-    "int32"
-    "uint32"
-    "int64"
-    "uint64"
-    "float32"
-    "float64"
-    "string"
-    "time"
-    "duration"))
+(declare msg-type->map)
 
-#_(defmethod from-java MessageType
+(defmulti parse-field class)
+
+(defmethod parse-field :default [field] (.getValue field))
+
+(defmethod parse-field ArrayType
+  [field]
+  (case (.getType field)
+    "int8[]"     (.getAsBytes field)
+    "char[]"     (.getAsShorts field)
+    "uint8[]"    (.getAsShorts field)
+    "int16[]"    (.getAsShorts field)
+    "uint16[]"   (.getAsInts field)
+    "int32[]"    (.getAsInts field)
+    "uint32[]"   (.getAsLongs field)
+    "int64[]"    (.getAsLongs field)
+    "uint64[]"   (.getAsBigIntegers field)
+    "float32[]"  (.getAsFloats field)
+    "float64[]"  (.getAsDoubles field)
+    "time[]"     (.getAsTimestamps field)
+    "duration[]" (.getAsDurations field)))
+
+(defmethod parse-field MessageType
+  [field]
+  (msg-type->map field))
+
+(defn msg-type->map
   ([msg-type]
-   (loop [[field-names & field-names] (.getFieldNames msg-type) ret (transient {})]
-     (let [field (.getField msg-type field-name)
-           field-type (.getType field)]
-       (if (nil? field-names)
-         (persistent! ret)
-         (recur field-names (assoc! ret (keyword field-name) (parse-field field-type field))))))))
+   (persistent!
+    (reduce (fn [ret field-name]
+              (let [field (.getField msg-type field-name)
+                    field-type (.getType field)]
+                (assoc! ret (keyword field-name) (parse-field field))))
+            (transient {})
+            (.getFieldNames msg-type)))))
 
-#_(defmethod from-java BagMessage
-  ([msg] (->RosMessage (.-topic msg)
-                       (from-java (.-msg msg))
-                       (.-timestamp msg))))
+(defn from-bag-msg
+  ([msg] (->RosMessage (.getTopic msg)
+                       (.getMessage msg)
+                       (.getTimestamp msg))))
 
 (defn open
   "Open a bag. Can be a string representing a path to a bag file, or
@@ -45,16 +54,22 @@
   (doto (BagFile. bag) (.read)))
 
 (defn read-messages
-  ""
+  "Read messages from bag."
   ([^BagFile bag]
    (read-messages bag (map #(.getName %) (.getTopics bag))))
   ([^BagFile bag topics]
-   (iterator-seq (.iterMessagesOnTopics bag (java.util.ArrayList. topics)))))
+   (map from-bag-msg (iterator-seq (.iterMessagesOnTopics bag (java.util.ArrayList. topics))))))
 
 (comment
   (do
     (import '[java.io File])
     (def file (File. "resources/planar_lidar.bag"))
     (def bag (open (.getPath file)))
-    (first msgs) 
-    (def msgs (read-messages bag))))
+    (def msgs (read-messages bag))
+    (def msg (:message (first msgs)))
+    (def field-names (.getFieldNames msg))
+    (def angle-increment (.getField msg "angle_increment"))
+    (def angle-increment-type (.getType angle-increment))
+    (def ranges (.getField msg "ranges"))
+    (def ranges-type (.getType ranges))
+    (msg-type->map msg)))
