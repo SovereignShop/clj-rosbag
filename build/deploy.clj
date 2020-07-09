7dfca503f0012102402e33d2a847901569cd1741
John Collins <john.collins@braincorp.com>
2020-07-09 01:42:05 -0700

    Update deploy

--- build/deploy.clj
+++ build/deploy.clj
@@ -1,29 +1,35 @@
 (ns deploy
   (:require
-   [badigeon.compile :as compile]
+   [badigeon.exec :as exec]
    [badigeon.prompt :as prompt]
    [badigeon.sign :as sign]
    [badigeon.deploy :as deploy]
    [badigeon.jar :as jar]
+   [clojure.string :as s]
    [clojure.tools.deps.alpha.reader :as deps-reader]))
 
 (defn deploy-lib []
-  (let [deps (:deps (deps-reader/slurp-deps "deps.edn"))]
-    (jar/jar 'org.cartesiantheatrics/clj-rosbag {:mvn/version "0.0.1"}
-             {:out-path                "target/clj-rosbag-0.0.1.jar"
+  (let [deps    (:deps (deps-reader/slurp-deps "deps.edn"))
+        version (s/trim (with-out-str
+                          (exec/exec "git" {:proc-args ["describe" "--tags"]
+                                            ;; The error message of the exception thrown upon error.
+                                            :error-msg "Failed to get tags"})))]
+    (assert (re-find #"^\d.\d.\d$" version))
+    (jar/jar 'org.cartesiantheatrics/clj-rosbag {:mvn/version version}
+             {:out-path                (format "target/clj-rosbag-%s.jar" version)
               :paths                   ["src/clj"]
               :deps                    deps
               :mvn/repos               '{"clojars" {:url "https://repo.clojars.org/"}}
               :exclusion-predicate     jar/default-exclusion-predicate
               :allow-all-dependencies? true})
-    (let [artifacts (-> [{:file-path "target/clj-rosbag-0.0.1.jar"
+    (let [artifacts (-> [{:file-path (format "target/clj-rosbag-%s.jar" version)
                           :extension "jar"}
                          {:file-path "pom.xml"
                           :extension "pom"}]
                         (badigeon.sign/sign {:command "gpg"}))
           password  (badigeon.prompt/prompt-password "Password: ")]
       (badigeon.deploy/deploy 'org.cartesiantheatrics/clj-rosbag
-                              "0.0.1"
+                              version
                               artifacts
                               {:url "https://repo.clojars.org/"}
                               {:credentials     {:username "cartesiantheatrics"
